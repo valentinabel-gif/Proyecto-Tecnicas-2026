deb05e83169a389e6e82b254d2b12b258b5393a9
sanaguavalentinabelen@gmail.com <sanaguavalentinabelen@gmail.com>
2026-05-06 21:58:14 -0300

    Nuevo repo

---
+++ BE-Libreria/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_Libreria
+{
+    public class Class1
+    {
+    }
+}
