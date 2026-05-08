db25e3de1d0d939b5e60b506df4f4def0698b9a2
sanaguavalentinabelen@gmail.com <sanaguavalentinabelen@gmail.com>
2026-05-08 20:23:11 -0300

    Desarrollo de las primeras pantallas de encargado

--- UI-Libreria/Program.cs
+++ UI-Libreria/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UI_Libreria.EncargadoDeStock;
 
 namespace UI_Libreria
 {
@@ -16,7 +17,7 @@ namespace UI_Libreria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new InicioEncargadoStock());
         }
     }
 }
