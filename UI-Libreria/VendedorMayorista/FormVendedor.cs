573460ffbdb759150b681350858edcd10d207b5d
Tiago Dimaso <tu-email@ejemplo.com>
2026-06-05 09:10:11 -0300

    Cambio en Form Vendedor

--- UI-Libreria/VendedorMayorista/FormVendedor.cs
+++ UI-Libreria/VendedorMayorista/FormVendedor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BLL_Libreria;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -66,7 +67,15 @@ namespace UI_Libreria
 
         private void button6_Click(object sender, EventArgs e)
         {
+            DialogResult resultado = MessageBox.Show("¿Está seguro que desea cerrar sesión?", "Cerrar sesión", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
+            if (resultado == DialogResult.Yes)
+            {
+                Sesion.Instancia.CerrarSesion(); // Singleton limpia la sesión
+                login loginForm = new login();  // abrís el form de login
+                loginForm.Show();
+                this.Close();
+            }
         }
 
         private void LimpiarPanel()
--- UI-Libreria/VendedorMayorista/UcConsultarPrecio.Designer.cs
+++ UI-Libreria/VendedorMayorista/UcConsultarPrecio.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UcConsultarPrecio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UcConsultarPrecio";
+            this.Load += new System.EventHandler(this.UcConsultarPrecio_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
